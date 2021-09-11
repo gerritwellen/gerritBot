22cb7757ec697ef6fddcc698ff6dad594644bae8
Gerrit Wellen <58791333+gerritwellen@users.noreply.github.com>
2021-09-11 20:22:04 +0200

    Added Allowed Channels Option to Config

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,19 @@
 module.exports = (client, msg) => {
   // Ignore all bots
   if (msg.author.bot) return;
-
-  if (msg.channelId == client.config.blockedChannels) return;
-
+  //  Check for blocked/allowed channels
+  if (
+    client.config.blockedChannels.length == 0 &&
+    msg.channelId == client.config.blockedChannels
+  ) {
+    return;
+  }
+  if (
+    client.config.allowedChannels.length == 0 &&
+    msg.channelId !== client.config.allowedChannels
+  ) {
+    return;
+  }
   // Ignore messages not starting with the prefix (in config.json)
   if (msg.content.charAt(0) !== "!") return;
 
