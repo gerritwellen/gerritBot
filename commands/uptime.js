bc2e631073b4aab51f6185df1c166b65cfe8d6ee
Gerrit Wellen <58791333+gerritwellen@users.noreply.github.com>
2021-09-12 22:32:41 +0200

    Updated Uptime Commands

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,20 +3,17 @@ const prettyMs = require("pretty-ms");
 
 exports.run = (client, msg, args) => {
   let uptimeEmbed = new MessageEmbed()
-    .setTitle(`Information about Bot Uptime`)
     .addField(
-      `**${client.user.username}**'s Uptime`,
+      `Uptime of **${client.user.username}**`,
       `${prettyMs(client.uptime, {
         secondsDecimalDigits: 0,
       })}`,
       true
     )
     .setColor(
-      msg.author.displayHexColor === "#000000"
+      client.user.displayHexColor === "#000000"
         ? "#FFFFFF"
-        : msg.author.displayHexColor
-    )
-    .setFooter(`ID: ${msg.author.id}`)
-    .setTimestamp(new Date());
+        : client.user.displayHexColor
+    );
   msg.channel.send({ embeds: [uptimeEmbed] });
 };
